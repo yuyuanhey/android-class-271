c746b88a3f72ac4d76760784556c4984d395c2e7
FrankLikuohao <frank.likuohao@gmail.com>
2016-07-26 10:36:26 +0800

    add button

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText("Hello Android!");
+    }
+    public void submit(TextView textView){
+        textView.setText("Hi Android!");
     }
 }
