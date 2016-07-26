3d8e260c544668ff87be1c7f73487dcd4579c64c
FrankLikuohao <frank.likuohao@gmail.com>
2016-07-26 11:46:57 +0800

    change the image of button
    add two radiobutton and improve the display of the submit button

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,19 +2,41 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    EditText editText;
+    RadioGroup radioGroup;
+    String sex = "male";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.textView);
-        textView.setText("Hello Android!");
+        editText = (EditText)findViewById(R.id.editText);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup Group, int isChecked){
+                if(isChecked == R.id.maleRadioButton){
+                    sex = "male";
+                }
+                else if(isChecked == R.id.femaleRadioButton){
+                    sex = "female";
+                }
+            }
+        });
     }
-    public void submit(TextView textView){
-        textView.setText("Hi Android!");
+    public void submit(View view){
+        String text = editText.getText().toString();
+        text = text + "  " + sex;
+        textView.setText(text);
+        editText.setText("");
     }
 }
